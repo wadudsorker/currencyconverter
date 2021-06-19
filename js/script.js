67a7f8e60823729742901e1b2d23d15d1e3df1a7
wadudsorker <85513189+wadudsorker@users.noreply.github.com>
2021-06-19 21:21:24 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var crrncy = {'EUR': {'INR': 87.97,'USD': 0.84, 'BDT': 100.53},
+'USD': {'INR': 74.14, 'EUR': 0.84, 'BDT': 81.546},
+'BDT': {'INR': 0.79, 'EUR': 0.00099, 'EUR': 0.0083, }};
+var btn = document.querySelector('.calculate-btn');
+
+var baseCurrencyInput = document.getElementById('currency-1');
+var secondCurrencyInput = document.getElementById('currency-2');
+var amountInput = document.getElementById('amount');
+var toShowAmount = document.querySelector('.given-amount');
+var toShowBase = document.querySelector('.base-currency');
+var toShowSecond = document.querySelector('.second-currency');
+var toShowResult = document.querySelector('.final-result');
+
+function convertCurrency(event) {
+  event.preventDefault();
+  var amount = amountInput.value;
+  var from = baseCurrencyInput.value;
+  var to = secondCurrencyInput.value;
+  var result = 0;
+  
+  try{
+    if (from == to){
+      result = amount;
+    } else {
+     result = amount * crrncy[from][to];
+  }
+  }
+  catch(err) {
+    result = amount * (1 / crrncy[to][from]);
+  }
+  
+  toShowAmount.innerHTML = amount;
+  toShowBase.textContent = from + ' = ';
+  toShowSecond.textContent = to;
+  toShowResult.textContent = result; 
+}
+
+btn.addEventListener('click', convertCurrency);
+
